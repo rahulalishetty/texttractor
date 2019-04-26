23c26236f3d9f191faa6902421667a440621e674
rahul alishetty <rahulalishetty1905@gmail.com>
2019-04-26 16:17:35 +0530

    add phone validation and change state

--- src/store/reducers/Reducer.js
+++ src/store/reducers/Reducer.js
@@ -3,10 +3,8 @@ import { sipHangup } from "../../utils/sipUtils";
 
 const initialState = {
   call: null,
-  contacts: null,
   onGoingCall: false,
   phoneNumber: null,
-  showModal: false,
   callerName: null,
   transcript: null,
   summary: null
@@ -14,12 +12,6 @@ const initialState = {
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
-    case actionTypes.ADD_CONTACTS:
-      console.log("contacts", action.payload);
-      return {
-        ...state,
-        contacts: action.payload
-      };
     case actionTypes.PLACE_CALL:
       return {
         ...state,
@@ -27,11 +19,6 @@ const reducer = (state = initialState, action) => {
         onGoingCall: true,
         phoneNumber: action.payload.phone,
         callerName: action.payload.name
-      };
-    case actionTypes.ADD_NEW_CONTACT:
-      return {
-        ...state,
-        contacts: action.payload
       };
     case actionTypes.GOTO_HOME:
       sipHangup();
